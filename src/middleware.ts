3960a91c284ef9de97741fc7f195a079d4acace4
jony-jr <ahmed.wael28100@gmail.com>
2025-09-27 19:28:14 +0300

    handle cookie in prod

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,15 +2,16 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest) {
+    const cookieName = process.env.NODE_ENV === 'production' ? '__Secure-next-auth.session-token' : 'next-auth.session-token'
     const jwt = await getToken({
-        req
+        req,cookieName
     })
     // console.log("🚀 ~ middleware ~ jwt:", jwt)
-    if(jwt){
+    if (jwt) {
         return NextResponse.next()
     }
-  return NextResponse.redirect(`${process.env.MY_DOMAIN}/login`)
+    return NextResponse.redirect(`${process.env.MY_DOMAIN}/login`)
 }
-export const config ={
-    matcher:['/cart:path*','/allorders']
+export const config = {
+    matcher: ['/cart:path*', '/allorders']
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,11 +1,13 @@
 import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
-export async function getAuthUserToken():Promise<string>{
+export async function getAuthUserToken(): Promise<string> {
     const cookie = await cookies()
-    const sessionToken = cookie.get('next-auth.session-token')?.value
-    const tkn = await decode({token:sessionToken, secret: process.env.NEXTAUTH_SECRET||''} )
+    const sessionToken =
+        cookie.get('next-auth.session-token')?.value ||
+        cookie.get('__Secure-next-auth.session-token')?.value
+    const tkn = await decode({ token: sessionToken, secret: process.env.NEXTAUTH_SECRET || '' })
     // console.log("🚀 ~ getAuthUserToken ~ tkn:", tkn)
-    const userToken:string|null = tkn?.userTKN
+    const userToken: string | null = tkn?.userTKN
     return userToken;
 }
